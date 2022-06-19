4c2a7d1017fb14a487cbc1209f8cd8a2972b275c
Eme-Hache <luisalfonsodsg@outlook.com>
2022-06-18 20:47:37 -0500

    not found page

---
+++ src/pages/public/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div className='h-screen w-screen flex justify-center items-center flex-col gap-4'>
+            <h1 className='text-xl md:text-3xl'>Página no encontrada :(</h1>
+        </div>
+    )
+}
+
+export default NotFound
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,23 @@ import { Routes, Route } from 'react-router-dom'
 
 import privateRoutes, { Auth } from './private'
 import publicRoutes, { UnAuth } from './public'
+import NotFound from '../pages/public/NotFound'
 
 export default () => {
     return (
         <Routes key={'project-routes'}>
+            {/* Private routes */}
+            <Route path='/projects' element={<Auth />}>
+                {privateRoutes.map((route, index) => (
+                    <Route
+                        index={route.index}
+                        path={route.path}
+                        element={<route.component />}
+                        key={index + '-private-route'}
+                    />
+                ))}
+            </Route>
+
             {/* Public routes */}
             <Route path='/' element={<UnAuth />}>
                 {publicRoutes.map((route, index) => (
@@ -18,17 +31,8 @@ export default () => {
                 ))}
             </Route>
 
-            {/* Private routes */}
-            <Route path='/projects' element={<Auth />}>
-                {privateRoutes.map((route, index) => (
-                    <Route
-                        index={route.index}
-                        path={route.path}
-                        element={<route.component />}
-                        key={index + '-private-route'}
-                    />
-                ))}
-            </Route>
+            {/* Not found */}
+            <Route path='*' element={<NotFound />} />
         </Routes>
     )
 }
